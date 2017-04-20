071f8b63cb0d244cc6f45d7174b42ec6518b198d
antzu <andres.aava@gmail.com>
2017-04-20 22:26:43 +0300

    Updated script to automatically select dates

--- market_data_scriptpy.py
+++ market_data_scriptpy.py
@@ -10,12 +10,14 @@ import pandas as pd
 import datetime
 
 #Create datelist
-date = datetime.datetime.today()
-n = 2600
+dt = datetime.datetime.today()
+n = 2800
+dp = dt - datetime.timedelta(days = n)
 datelist = []
 for x in range (0, n):
-    d = (date - datetime.timedelta(days = x))
+    d = (dt - datetime.timedelta(days = x))
     datelist.append(d.strftime('%Y-%m-%d'))
+
 
 # Load dataset
 stocks = ["AAPL", "TSLA", "QCOM", "INTU"]
@@ -24,11 +26,10 @@ alldata['Date'] = datelist
 
 #Extract data to set
 for stock in stocks:
-    url = "http://chart.finance.yahoo.com/table.csv?s="+stock+"&a=3&b=20&c=2010&d=3&e=20&f=2017&g=d&ignore=.csv"
+    url = "http://chart.finance.yahoo.com/table.csv?s="+stock+"&a="+str(int(dp.month - 1))+"&b="+str(dp.day)+"&c="+str(dp.year)+"&d="+str(int(dt.month - 1))+"&e="+str(dt.day)+"&f="+str(dt.year)+"&g=d&ignore=.csv"
     dataset = pd.read_csv(url, index_col=None)     
     dataset = dataset.set_index('Date')
     alldata[stock] = alldata['Date'].map(dataset['Close']).values
-    
     
 
     
